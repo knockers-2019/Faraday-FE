efe990e4a6a56ac64e641c744f7bac6340efe322
Nikolaj Dyring <nikodyring@gmail.com>
2019-11-21 20:13:50 +0100

    Revert "Revert ".Core project added.""
    This reverts commit 6558bb6ba872ec1eb4fc900a0033711e1a32a720.

--- FaradayFE/FaradayFE/Controllers/HomeController.cs
+++ FaradayFE/FaradayFE/Controllers/HomeController.cs
@@ -1,44 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FaradayFE.Models;
 
 namespace FaradayFE.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public IActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult CancelBooking()
+        public IActionResult CancelBooking()
         {
             ViewBag.Message = "Cancel booking";
-
             return View();
         }
 
-        public ActionResult CreateBooking()
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Our contact page.";
+            return View();
+        }
+
+        public IActionResult CreateBooking()
         {
             ViewBag.Message = "Cancel booking";
-
             return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
