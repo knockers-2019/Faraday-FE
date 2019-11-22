79cd82152ae085345c10bf2bc795c385cf74c59c
JonasManley <jonasmanley@hotmail.com>
2019-11-22 20:13:27 +0100

    project

---
+++ FaradayFE/FaradayFE/Models/LocationObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaradayFE.Models
+{
+    public class LocationObject
+    {
+        int Id = 1;
+        string Address = "2";
+        string Zipcode = "3";
+        string City = "4";
+        string Country = "5";
+
+        public LocationObject()
+        {
+
+        }
+
+        public LocationObject(int Id, string Addres, string Zipcode, string City, string Country)
+        {
+            this.Id = Id;
+            this.Address = Addres;
+            this.Zipcode = Zipcode;
+            this.City = City;
+            this.Country = Country;
+        }
+
+        private int myVar;
+
+        public int getId
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        public string getAddress
+        {
+            get { return Address; }
+            set { Address = value; }
+        }
+        public string getZipcode
+        {
+            get { return Zipcode; }
+            set { Zipcode = value; }
+        }
+        public string getCity
+        {
+            get { return City; }
+            set { City = value; }
+        }
+        public string getCountry
+        {
+            get { return Country; }
+            set { Country = value; }
+        }
+
+    }
+}
--- FaradayFE/FaradayFE/protobufferrepo/Service.cs
+++ FaradayFE/FaradayFE/protobufferrepo/Service.cs
@@ -1,26 +1,30 @@
 ﻿using Faraday_BE_gRPC;
 using Grpc.Core;
+using Grpc.Net.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace FaradayFE.Protofil
+namespace FaradayFE.protobufferrepo
 {
     public class Service
     {
+
         private Booking.BookingClient client;
         public Service()
         {
-            Channel channel = new Channel("localhost", ChannelCredentials.Insecure);
-            client = new Booking.BookingClient(channel);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var bookingClient = new Booking.BookingClient(channel);
         }
 
+        /// <summary>
+        /// Property to use the Proto methods. 
+        /// </summary>
         public Booking.BookingClient Client
         {
             get { return client; }
             set { client = value; }
-         }
-         
+        }
     }
 }
