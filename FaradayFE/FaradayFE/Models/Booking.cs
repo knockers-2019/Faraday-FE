9e38811dde2ac3d481844cb1457e63065f319b0d
DunkRing <Chris31@live.dk>
2019-11-24 22:38:51 +0100

    dummy booking

---
+++ FaradayFE/FaradayFE/Models/Booking.cs
@@ -0,0 +1,43 @@
+﻿using FaradayFE.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaradayFE.Models
+{
+    public class Booking
+    {
+        private Car car;
+        private string customer;
+        private string pickUpDate;
+
+        public Booking()
+        {
+                
+        }
+        public Booking(Car car, string customer, string pickUpDate)
+        {
+            this.car = car;
+            this.customer = customer;
+            this.pickUpDate = pickUpDate;
+        }
+
+        public Car Car
+        {
+            get { return car; }
+            set { car = value; }
+        }
+        public string Customer
+        {
+            get { return customer; }
+            set { customer = value; }
+        }
+
+        public string PickUpDate
+        {
+            get { return pickUpDate; }
+            set { pickUpDate = value; }
+        }
+    }
+}
